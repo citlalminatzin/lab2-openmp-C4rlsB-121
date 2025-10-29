821d0de6f84b11b80a75cc72959c9b0846cd0219
Carlos Barrera <carlosbarrerac121@ciencias.unam.mx>
2025-10-28 20:39:12 -0600

    Create holaMundo.c
    Implementación de Hola Mundo con OpenMP

---
+++ HolaMundo/holaMundo.c
@@ -0,0 +1,22 @@
+#inlude <omp.h>
+#include <stdio.h>
+
+int main(void){
+        float INICIO, FIN, tiempof;
+        int numThreads;
+
+        INICIO = omp_get_wtime();
+        #pragma omp parallel
+        {
+        int thread = omp_get_thread_num();
+        printf("Wnas mundo, desde el hilo: %d\n", thread);
+                #pragma omp master
+                {
+                numThreads = omp_get_num_threads();
+                }
+        }
+        FIN = omp_get_wtime();
+        tiempof = FIN - INICIO;
+        printf("Tiempo de ejecucion con %d hilos: %f\n", numThreads, tiempof);
+        return 0;
+}
